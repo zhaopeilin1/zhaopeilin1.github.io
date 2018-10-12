4dd3360a844785113362ca52715a4e21d42fe215
zhaopeilin1 <284340211@qq.com>
2018-10-12 15:12:54 +0000

    aa

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # zhaopeilin1.github.ioaabb
         inet 192.168.122.1  netmask 255.255.255.0  broadcast 192.168.122.255
         inet 192.168.2.104  netmask 255.255.255.0  broadcast 192.168.2.255
+        inet 192.168.2.102  netmask 255.255.255.0  broadcast 192.168.2.255
--- autoPush.sh
+++ autoPush.sh
@@ -1,6 +1,6 @@
  #/bin/bash
-cd /home/zhaopeilin/data01/code/all.git/personal/zhaopeilin1.github.io
-ifconfig | grep 192 >> README.md 
+cd /home/pi/all.git/zhaopeilin1.github.io
+ifconfig | grep broadcast >> README.md 
 git add README.md && git  commit -m aa && git push
 
 
