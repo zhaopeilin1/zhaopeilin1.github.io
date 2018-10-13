0b7ec9d4ef8558b3df5c7097f78a933c19c0f4ba
zhaopeilin1 <284340211@qq.com>
2018-10-13 17:09:11 +0800

    pull

--- autoPush.sh
+++ autoPush.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 cd /home/pi/all.git/zhaopeilin1.github.io
+git checkout addr.txt && git pull
 ifconfig | grep broadcast >> addr.txt
-git pull && git add addr.txt && git  commit -m ip && git push
+git add addr.txt && git  commit -m ip && git push
 
 
